a71fddfe8677b8b64fa6acbd4aed8a899ed7cf64
Bryce Olcott <10872719@uvu.edu>
2023-08-25 22:10:25 -0600

    coded in a functional movement system

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Camera playerCamera;
+    private Rigidbody2D rb;
+    private Vector2 playerMoveVector;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float playerSpeed;
+    [SerializeField] private InputActionAsset inputAction;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        inputAction = GetComponent<InputActionAsset>();
+        inputAction.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+    void OnJump()
+    {
+        rb.velocity = Vector2.up * jumpForce;
+    }
+
+    void OnMove(InputValue value)
+    {
+        playerMoveVector = value.Get<Vector2>();
+        
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(playerMoveVector.x * playerSpeed,rb.velocity.y);
+    }
+}
