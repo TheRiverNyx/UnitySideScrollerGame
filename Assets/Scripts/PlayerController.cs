f0458aecec10dec0c993ee8522db266a2f245bac
Bryce Olcott <bryceolcott@gmail.com>
2023-08-31 03:22:50 -0600

    added conditionals to, and refined, movement system.
    I also added a drag system and changed the behavior of the playerInput component on the player.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,35 +11,66 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 playerMoveVector;
     [SerializeField] private float jumpForce;
-    [SerializeField] private float playerSpeed;
-    [SerializeField] private InputActionAsset inputAction;
+    [SerializeField] private float playerAcceleration;
+    [SerializeField] private float maxPlayerSpeed;
+    [SerializeField] private float dragForce;
+    [SerializeField] private float defaultDrag;
+    [SerializeField] private Transform groundChecker;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+    [SerializeField] private float checkRadius;
+    private bool isPlayerMoving;
     
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        inputAction = GetComponent<InputActionAsset>();
-        inputAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
     }
-    void OnJump()
+    public void OnJump(InputAction.CallbackContext context)
     {
-        rb.AddForce(Vector2.up * jumpForce,ForceMode2D.Impulse);
+        if (isGrounded)
+        {
+            rb.drag = defaultDrag;
+            rb.AddForce(Vector2.up * jumpForce,ForceMode2D.Impulse);
+        }
     }
 
-    public void OnMove(InputValue value)
+    public void OnMove(InputAction.CallbackContext context)
     {
-        playerMoveVector = value.Get<Vector2>();
+        if (context.started)
+        {
+            isPlayerMoving = true;
+            rb.drag = defaultDrag;
+            playerMoveVector = context.ReadValue<Vector2>();
+        }else if (context.canceled)
+        {
+            playerMoveVector = context.ReadValue<Vector2>();
+            rb.drag = dragForce;
+            isPlayerMoving = false;
+        }
     }
     private void FixedUpdate()
     {
-        //rb.velocity = new Vector2(playerMoveVector.x * playerSpeed,rb.velocity.y);
-        rb.AddForce(new Vector2(playerMoveVector.x * playerSpeed, 0f));
-        
+        isGrounded = Physics2D.OverlapCircle(groundChecker.position, checkRadius, whatIsGround);
+        rb.AddForce(new Vector2(playerMoveVector.x * playerAcceleration, 0f));//I could just do player acceleration, but i like how this feels more
+        rb.velocity=Vector2.ClampMagnitude(rb.velocity, maxPlayerSpeed);
+        if (rb.velocity.x == 0f || !isGrounded)
+        {
+            rb.drag = defaultDrag;
+        }else if (!isPlayerMoving && isGrounded)
+        {
+            rb.drag = dragForce;
+        } else if (!isPlayerMoving && !isGrounded)
+        {
+            rb.drag = defaultDrag;
+        }
     }
 }
